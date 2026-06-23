3ea41591d8fa650053fabeb03545bfc20bfe073d
Tim Smith <tim.smith.hdg@gmail.com>
2026-06-23 11:35:40 -0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -40,7 +40,7 @@
         </div>
 
         <a class="story-paragraph start-btn" href="#intro" onclick={handleStart}>
-            Start
+            <span>Start</span>
             <svg class="bounce-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round">
                 <path d="M12 5v14M19 12l-7 7-7-7"/>
             </svg>
